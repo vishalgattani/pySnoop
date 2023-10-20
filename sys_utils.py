09882f6531aaea23b75a7ede9d5f6cad4fbdcee9
Vishal Gattani <vishalgattani09@gmail.com>
2023-10-20 12:29:40 -0400

    system monitoring functions

---
+++ sys_utils.py
@@ -0,0 +1,46 @@
+import psutil
+
+from logger import logger
+
+
+def check_cpu_usage(threshold=50):
+    cpu_usage = psutil.cpu_percent(interval=1)
+    if cpu_usage > threshold:
+        logger.warning(f"High CPU usage detected: {cpu_usage}%")
+    else:
+        logger.info(f"CPU usage detected: {cpu_usage}%")
+
+
+def check_memory_usage(threshold=80):
+    memory_usage = psutil.virtual_memory().percent
+    memory_free = psutil.virtual_memory().free
+    memory_used = psutil.virtual_memory().used
+    if memory_usage > threshold:
+        logger.warning(f"High memory usage detected: {memory_usage}%")
+    else:
+        logger.info(f"Memory usage detected: {memory_usage}%")
+
+
+def check_disk_space(path="/", threshold=75):
+    disk_usage = psutil.disk_usage(path).percent
+    if disk_usage > threshold:
+        logger.warning(f"Low disk space detected: {disk_usage}%")
+    else:
+        logger.info(f"Disk space detected: {disk_usage}%")
+
+
+def check_network_traffic(threshold=100 * 1024 * 1024):
+    network_traffic = (
+        psutil.net_io_counters().bytes_recv + psutil.net_io_counters().bytes_sent
+    )
+    if network_traffic > threshold:
+        logger.warning(f"High network traffic detected: {network_traffic:.2f} MB")
+    else:
+        logger.info(f"Network traffic detected: {network_traffic:.2f} MB")
+
+
+def run_health_checks():
+    check_cpu_usage()
+    check_memory_usage()
+    check_disk_space()
+    check_network_traffic()
